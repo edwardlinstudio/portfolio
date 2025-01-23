272bf34cf2785414db6842ce0d35d9e54d99ff84
Edward Lin <edward.lin000@gmail.com>
2025-01-23 18:34:07 +0000

    routing

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Photo from "./pages/Photo";
@@ -8,20 +10,28 @@ import CV from "./pages/CV";
 import NotFound from "./pages/NotFound";
 import './styles/App.css'
 
-function App() {
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
+function App() {
   return (
-    <BrowserRouter basename="/portfolio">
-      <Routes>
-        <Route path="/home" element={<Home />}/>
-        <Route path="/photo" element={<Photo />}/>
-        <Route path="/digital" element={<Digital />}/>
-        <Route path="/detail" element={<Detail />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/cv" element={<CV />}/>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter basename="/portfolio">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/photo" element={<Photo />}/>
+          <Route path="/digital" element={<Digital />}/>
+          <Route path="/detail" element={<Detail />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/cv" element={<CV />}/>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
